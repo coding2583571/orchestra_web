336fb119e8ec090d80b6d3164de1f4d1acbdc804
coding2583571 <97715381+coding2583571@users.noreply.github.com>
2023-08-19 10:59:35 -0700

    adding models

--- contact/models.py
+++ contact/models.py
@@ -6,8 +6,8 @@ class Contact(models.Model):
     job_title = models.CharField(blank=True, max_length=500)
     work_number = models.CharField(blank=True, max_length=500)
     alternate_number = models.CharField(blank=True, max_length=500)
-    work_email = models.CharField(blank=True, max_length=500)
-    alternate_email = models.CharField(blank=True, max_length=500)
+    work_email = models.EmailField(blank=True, max_length=500)
+    alternate_email = models.EmailField(blank=True, max_length=500)
     work_address = models.TextField(blank=True, max_length=500)
 
 # Create your models here.
