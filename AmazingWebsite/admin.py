bb4ab26f74513609604138237ce0ef3ad3858b5b
coding2583571 <97715381+coding2583571@users.noreply.github.com>
2023-08-19 05:36:39 -0700

    edits

--- AmazingWebsite/admin.py
+++ AmazingWebsite/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Heading
+
+class WebsiteTitleAdmin(admin.ModelAdmin):
+    list_display = ('Website_Title','Date')
+
+admin.site.register(Heading, WebsiteTitleAdmin)
 
 # Register your models here.
--- AmazingWebsite/models.py
+++ AmazingWebsite/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
+class Heading(models.Model):
+   Website_Title = models.CharField(max_length=1000, blank=True)
+   Date = models.DateField(blank=True)
 # Create your models here.
