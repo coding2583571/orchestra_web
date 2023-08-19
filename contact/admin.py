3ca4e671cdb352d0cfae55514ff2b26b2a58ec49
coding2583571 <97715381+coding2583571@users.noreply.github.com>
2023-08-18 17:24:48 -0700

    commit

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Contact
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('first_name','last_name','job_title','work_number','work_email')
+
+
+admin.site.register(Contact, ContactAdmin)
 
 # Register your models here.
