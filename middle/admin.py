d15595f3b1ed21ed2146642a9534619f7a7cf62d
coding2583571 <97715381+coding2583571@users.noreply.github.com>
2023-08-19 11:11:18 -0700

    more models

--- middle/admin.py
+++ middle/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Concert, Contest, Event
+
+class ConcertAdmin(admin.ModelAdmin):
+    list_display = ('concert_name', 'concert_date_and_time', 'Qualifying_Orchestras', 'Director_In_Charge', 'Director_Email')
+
+class ContestAdmin(admin.ModelAdmin):
+    list_display = ('contest_name','contest_date_and_time','Qualifying_Orchestras', 'Director_In_Charge', 'Director_Email')
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('event_name','event_date_and_time','Qualifying_Orchestras', 'Director_In_Charge', 'Director_Email')
 
 # Register your models here.
+admin.site.register(Concert, ConcertAdmin)
+admin.site.register(Contest, ContestAdmin)
+admin.site.register(Event, EventAdmin)
--- middle/models.py
+++ middle/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
+class Concert(models.Model):
+    concert_name = models.CharField(max_length=1050, blank=True)
+    concert_date_and_time = models.DateTimeField(blank=True)
+    location_address = models.TextField(blank=True, max_length=5000)
+    notes = models.TextField(blank=True, max_length=5000)
+    #add one for directors
+    Qualifying_Orchestras = models.TextField(blank=True,help_text='Which orchestras are involved/playing in this concert?', max_length=5000)
+    Director_In_Charge = models.CharField(max_length=500, blank=True)
+    Director_Email = models.EmailField(max_length=500, blank=True)
+    Director_Phone_Number = models.CharField(max_length=500, blank=True)
+
+class Contest(models.Model):
+    contest_name = models.CharField(max_length=1050, blank=True)
+    contest_date_and_time = models.DateTimeField(blank=True)
+    location_address = models.TextField(blank=True, max_length=5000)
+    notes = models.TextField(blank=True, max_length=5000)
+    #add one for directors
+    Qualifying_Orchestras = models.TextField(blank=True,help_text='Which orchestras are involved/playing in this concert?', max_length=5000)
+    # want to modify qualifying orchestra all three models - more like option based
+
+    Director_In_Charge = models.CharField(max_length=500, blank=True)
+    Director_Email = models.EmailField(max_length=500, blank=True)
+    Director_Phone_Number = models.CharField(max_length=500, blank=True)
+
+
+
+class Event(models.Model):
+    event_name = models.CharField(max_length=1050, blank=True)
+    event_date_and_time = models.DateTimeField(blank=True)
+    location_address = models.TextField(blank=True, max_length=5000)
+    notes = models.TextField(blank=True, max_length=5000)
+    #add one for directors
+    Qualifying_Orchestras = models.TextField(blank=True,help_text='Which orchestras are involved/playing in this concert?', max_length=5000)
+    Director_In_Charge = models.CharField(max_length=500, blank=True)
+    Director_Email = models.EmailField(max_length=500, blank=True)
+    Director_Phone_Number = models.CharField(max_length=500, blank=True)
+
 # Create your models here.
