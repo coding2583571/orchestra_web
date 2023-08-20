6f73a5b2191fda6e3461f0116a7c74edaca4b9e9
coding2583571 <97715381+coding2583571@users.noreply.github.com>
2023-08-20 16:10:41 -0700

    templates

--- orchestra_website/views.py
+++ orchestra_website/views.py
@@ -10,27 +10,37 @@ def web_homepage(request):
 
 def contact(request):
     website_title = Title.objects.all()
+    bootstrap = Bootstrap.objects.all()
+    links = Link.objects.all()
+    return render(request, 'contact/contact.html',{'website_title':website_title,'bootstrap':bootstrap,'links':links})
 
-    return render(request, 'contact/contact.html',{'website_title':website_title})
 
 def high(request):
     website_title = Title.objects.all()
+    bootstrap = Bootstrap.objects.all()
+    links = Link.objects.all()
+    return render(request, 'high/high.html',{'website_title':website_title,'bootstrap':bootstrap,'links':links})
 
-    return render(request, 'high/high.html',{'website_title':website_title})
 
 def middle(request):
     website_title = Title.objects.all()
+    bootstrap = Bootstrap.objects.all()
+    links = Link.objects.all()
+    return render(request, 'middle/middle.html',{'website_title':website_title,'bootstrap':bootstrap,'links':links})
 
-    return render(request, 'middle/middle.html',{'website_title':website_title})
 
 def intermediate(request):
     website_title = Title.objects.all()
+    bootstrap = Bootstrap.objects.all()
+    links = Link.objects.all()
+    return render(request, 'intermediate/intermediate.html',{'website_title':website_title,'bootstrap':bootstrap,'links':links})
 
-    return render(request, 'intermediate/intermediate.html',{'website_title':website_title})
 
 def dev_panel(request):
     website_title = Title.objects.all()
+    bootstrap = Bootstrap.objects.all()
+    links = Link.objects.all()
+    return render(request, 'ZZDev/dev_panel.html',{'website_title':website_title,'bootstrap':bootstrap,'links':links})
 
-    return render(request, 'ZZDev/dev_panel.html',{'website_title':website_title})
 
 # Create your views here.
